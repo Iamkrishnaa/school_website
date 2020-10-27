cb87a785f960943b3bf58396ea58389e4fa7e957
Krishna Adhikari <krishna.forkhaki@gmail.com>
2020-10-27 12:57:37 +0545

    inital commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//the below code will show loader until page loads
+document.onreadystatechange = function() { 
+    if (document.readyState !== "complete") { 
+        document.querySelector( 
+          "body").style.visibility = "hidden"; 
+        document.querySelector( 
+          "#loader").style.visibility = "visible"; 
+    } else { 
+        document.querySelector( 
+          "#loader").style.display = "none"; 
+        document.querySelector( 
+          "body").style.visibility = "visible"; 
+    } 
+};
