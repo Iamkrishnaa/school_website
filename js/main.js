27214a310421228d65e35156dd2fc98d4d562c27
Krishna Adhikari <krishna.forkhaki@gmail.com>
2020-11-03 11:37:25 +0545

    design modified

--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ AOS.init({
     duration: 1800,
     easing: 'ease-out-back',
     delay: 200,
-    offset: 500
+    offset: 500,
+    once: false,
+    anchorPlacement: 'bottom-bottom'
 });
 
 $('.counter').countUp({
